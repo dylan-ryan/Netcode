ce98f66ca259724adbfc3684a94f21caed0f88d0
Dylan <dylanryan431@gmail.com>
2025-03-05 01:11:34 -0400

    IPAdress field

--- Assets/Scripts/NetworkUI.cs
+++ Assets/Scripts/NetworkUI.cs
@@ -1,15 +1,36 @@
 using Unity.Netcode;
 using UnityEngine;
 using UnityEngine.UI;
+using Unity.Netcode.Transports.UTP;
 
 public class NetworkUI : MonoBehaviour
 {
     public Button hostButton;
     public Button clientButton;
+    public InputField ipAddressInput;
 
     void Start()
     {
-        hostButton.onClick.AddListener(() => NetworkManager.Singleton.StartHost());
-        clientButton.onClick.AddListener(() => NetworkManager.Singleton.StartClient());
+        hostButton.onClick.AddListener(StartHost);
+        clientButton.onClick.AddListener(StartClient);
+    }
+
+    void StartHost()
+    {
+        NetworkManager.Singleton.StartHost();
+    }
+
+    void StartClient()
+    {
+        string ipAddress = ipAddressInput.text;
+        if (!string.IsNullOrEmpty(ipAddress))
+        {
+            NetworkManager.Singleton.GetComponent<UnityTransport>().SetConnectionData(ipAddress, (ushort)7777);
+            NetworkManager.Singleton.StartClient();
+        }
+        else
+        {
+            Debug.LogError("Invalid IP");
+        }
     }
 }
